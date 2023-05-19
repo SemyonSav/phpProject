0f558b7b6823ba46488ed470325fe5554f1ae8a4
SemyonSav <59660695+SemyonSav@users.noreply.github.com>
2023-05-19 14:15:52 +0300

    INit

---
+++ localhost/main.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1 class="title">Name of Company</h1>
+        <nav></nav>
+    </header>
+    <main>
+        <div>
+            <div>
+                <p class="description">Описание</p>
+                <img src="ct.jfif" alt="Здесь был кот">
+                <a href="index2.html">Барсик</a>
+            </div>
+            <div>
+                <p class="description">Описание</p>
+                <img src="ct.jfif" alt="Здесь был кот">
+                <a href="index2.html">Барсик</a>
+            </div>
+            <div>
+                <p class="description">Описание</p>
+                <img src="ct.jfif" alt="Здесь был кот">
+                <a href="index2.html">Барсик</a>
+            </div>
+        </div>
+    </main>
+    <footer></footer>
+</body>
+</html>
